83d77fbe06df9556c772cec9a769a8e8ddf9f339
Purab07 <73265862+Purab07@users.noreply.github.com>
2020-11-29 16:24:16 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var speed,weight;
 var thickness;
 function setup() {
   createCanvas(1600,400);
-  //speed = random(223,321);
+  speed = random(223,321);
   weight = random(30,52);
   thickness = random(22,83);
   car = createSprite(50,200,50,50);
@@ -16,7 +16,7 @@ function draw() {
   bullet.velocityX =random(223,321);
   if (hasCollided(bullet,wall)){
     bullet.velocityX = 0;
-    var damage = 0.5*weight*speed*speed/(thickness*thichness*thickness);
+    var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
     if(damage>10){
       wall.shapeColor = color(255,0,0);
     }
@@ -27,7 +27,7 @@ function draw() {
   drawSprites();
 }
 
-function hasCollided(bullet,wall){
+function hasCollided(lbullet,lwall){
   bulletRightEdge = bullet.x + bullet.width;
   wallLeftEdge = lwall.x;
   if (bulletRightEdge>=wallLeftEdge){
